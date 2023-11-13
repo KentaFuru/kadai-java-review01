b244aa221c13733903de2ae3ea20a44972731a15
KentaFuru <4jfjatk9KV@protonmail.com>
2023-11-14 03:35:36 +0900

    レッスン3の課題です。

---
+++ src/Review01.java
@@ -0,0 +1,15 @@
+
+public class Review01 {
+
+    public static void main(String[] args) {
+        int num1 = 1000;
+        int num2 = 10;
+        int result;
+        result = tax(num1, num2);
+        System.out.println(num1 + "円の商品の税込価格は" + result + "円（消費税は" + (num1 / num2) + "円）です。");
+    }
+    public static int tax(int num1, int num2) {
+        int result = num1 + (num1 / num2);
+        return result;
+    }
+}

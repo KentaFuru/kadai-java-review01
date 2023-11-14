7b7970a1bb27cc99053a068279158e70c9b14797
KentaFuru <4jfjatk9KV@protonmail.com>
2023-11-14 16:29:44 +0900

    ご指摘箇所の修正を行いました。

--- src/Review01.java
+++ src/Review01.java
@@ -6,10 +6,10 @@ public class Review01 {
         int num2 = 10;
         int result;
         result = tax(num1, num2);
-        System.out.println(num1 + "円の商品の税込価格は" + result + "円（消費税は" + (num1 / num2) + "円）です。");
+        System.out.println(num1 + "円の商品の税込価格は" + (num1 + result) + "円（消費税は" + (num1 / num2) + "円）です。");
     }
     public static int tax(int num1, int num2) {
-        int result = num1 + (num1 / num2);
+        int result = num1 / 100 * num2;
         return result;
     }
 }

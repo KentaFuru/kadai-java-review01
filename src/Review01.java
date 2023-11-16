09c9ff11fa46110ed2c6a8c365192cc4d4cfaf2c
KentaFuru <4jfjatk9KV@protonmail.com>
2023-11-16 20:45:40 +0900

    taxメソッドの計算順を見直し、100未満の金額でも消費税が0にならないようにしました。meinメソッド側の出力部分で計算ではなくtaxメソッドからの戻り値を出力するように変更しました。

--- src/Review01.java
+++ src/Review01.java
@@ -2,14 +2,14 @@
 public class Review01 {
 
     public static void main(String[] args) {
-        int num1 = 1000;
+        int num1 = 50;
         int num2 = 10;
         int result;
         result = tax(num1, num2);
-        System.out.println(num1 + "円の商品の税込価格は" + (num1 + result) + "円（消費税は" + (num1 / num2) + "円）です。");
+        System.out.println(num1 + "円の商品の税込価格は" + (num1 + result) + "円（消費税は" + result + "円）です。");
     }
     public static int tax(int num1, int num2) {
-        int result = num1 / 100 * num2;
+        int result = num1 * num2 / 100;
         return result;
     }
 }
